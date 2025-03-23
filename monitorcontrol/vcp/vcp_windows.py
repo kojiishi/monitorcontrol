c5da79449128d6c0b855bede6e03b73cd9ea8a82
Koji Ishii <kojii@chromium.org>
2025-03-23 16:38:21 +0900

    Fixes

--- monitorcontrol/vcp/vcp_windows.py
+++ monitorcontrol/vcp/vcp_windows.py
@@ -166,7 +166,7 @@ if sys.platform == "win32":
             )
 
         @staticmethod
-        def _EnumDisplayMonitors() -> List[HMONITOR]:
+        def _get_hmonitors() -> List[HMONITOR]:
             """
             Calls the Windows `EnumDisplayMonitors` API in Python-friendly form.
             """
@@ -189,7 +189,7 @@ if sys.platform == "win32":
             return hmonitors
 
         @staticmethod
-        def _GetPhysicalMonitorsFromHMONITOR(
+        def _physical_monitors_from_hmonitor(
             hmonitor: HMONITOR,
         ) -> List[Tuple[HANDLE, str]]:
             """
@@ -246,7 +246,7 @@ if sys.platform == "win32":
             VCPError: Failed to enumerate VCPs.
         """
         physical_monitors = WindowsVCP._get_physical_monitors(
-            WindowsVCP._EnumDisplayMonitors, WindowsVCP._GetPhysicalMonitorsFromHMONITOR
+            WindowsVCP._get_hmonitors, WindowsVCP._physical_monitors_from_hmonitor
         )
         return list(
             WindowsVCP(handle, description)
